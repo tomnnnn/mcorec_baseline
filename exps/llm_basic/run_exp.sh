82f6b7818e871b30c7f8b17667069aea92c018af
Hai Phong Nguyen <tomn@skirit.metacentrum.cz>
2025-11-23 17:22:01 +0100

    llm experiment job scripts fixes

--- exps/llm_basic/run_exp.sh
+++ exps/llm_basic/run_exp.sh
@@ -3,16 +3,16 @@
 # This script is used to run the LLM basic experiment
 
 MODELS=(
-        "Qwen/Qwen3-8B-AWQ"
+#        "Qwen/Qwen3-8B-AWQ"
         "meta-llama/Llama-3.2-3B-Instruct"
-        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+#        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
 )
 
 mkdir -p logs
 
 for MODEL in "${MODELS[@]}"; do
 	MODEL_NAME=$(basename $MODEL)
-	qsub -v "MODEL=$MODEL" job.sh -o "logs/${MODEL_NAME}.out" -e "logs/${MODEL_NAME}.err" -N "llm_basic_${MODEL_NAME}"
+	qsub -v "MODEL=$MODEL,HF_TOKEN=$HF_TOKEN" -o "logs/${MODEL_NAME}.out" -e "logs/${MODEL_NAME}.err" -N "llm_basic_${MODEL_NAME}" job.pbs
 
 	echo "Submitted job for model: $MODEL"
 done

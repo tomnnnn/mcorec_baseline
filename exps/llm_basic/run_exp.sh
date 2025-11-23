186f409d466cb2be99c1ec4681fe16f302aef3ef
Hai Phong Nguyen <xnguye28@stud.fit.vutbr.cz>
2025-11-23 14:44:39 +0100

    implement basic llm experiment scripts

--- exps/llm_basic/run_exp.sh
+++ exps/llm_basic/run_exp.sh
@@ -3,12 +3,15 @@
 # This script is used to run the LLM basic experiment
 
 OUTPUT_DIR="outputs/$EXP_NAME"
-MODEL=$1
-LOG_FILE="$OUTPUT_DIR/exp-{MODEL}.log"
+MODELS=(
+        "Qwen/Qwen3-8B-AWQ",
+        "meta-llama/Llama-3.2-3B-Instruct",
+        "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+)
 
 mkdir -p $OUTPUT_DIR
 
-echo "Starting experiment with model: $MODEL"
-
-cd ../../
-python scripts/cluster_llm.py --model $MODEL
+for MODEL IN "${MODELS[@]}"; do
+	qsub -v "MODEL=$MODEL" job.sh -o "logs/${MODEL}.out" -e "logs/${MODEL}.err" -N "llm_basic_$(basename $MODEL)"
+	echo "Submitted job for model: $MODEL"
+done
